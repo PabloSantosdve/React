cb7eabd49bea44016969f0e25ac212a04d776e9c
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-04-15 12:40:22 -0300

    .

---
+++ desafio_prof/src/App.jsx
@@ -0,0 +1,27 @@
+import astronauta from './assets/astronauta.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+  
+        <a href="https://react.dev" target="_blank">
+          <img src={astronauta} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Obaaaa meu primeiro APP em React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        <a href="https://www.notion.so/Come-ando-em-Dispositivos-M-veis-d1a7350b4be4465f85b9e32dc573356a" target='_blank' >Hello world</a>
+      </p>
+    </>
+  )
+}
+
+export default App
