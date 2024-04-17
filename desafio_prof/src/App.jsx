949bb10ed2f5e98ec7ad58da11390a5e3ced1984
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-04-17 11:38:43 -0300

    .

--- desafio_prof/src/App.jsx
+++ desafio_prof/src/App.jsx
@@ -1,25 +1,28 @@
-import astronauta from './assets/astronauta.jpg'
 import './App.css'
+import jordan from '../src/assets/imagens/jordan.jpg' //está trazendo a imagem
+import michael from '../src/assets/imagens/jordan2.jpg'
+import Titulo from '../src/assets/Componentes/Titulo' // esta trazendo o componente header
+
 
 function App() {
+  const nome = "Pablão"
+  
+  function somatoria(a,b){
+    return a + b
+  }
 
+  
   return (
     <>
-      <div>
-  
-        <a href="https://react.dev" target="_blank">
-          <img src={astronauta} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Obaaaa meu primeiro APP em React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        <a className='link' href="https://www.notion.so/Come-ando-em-Dispositivos-M-veis-d1a7350b4be4465f85b9e32dc573356a" target='_blank' >Hello world</a>
-      </p>
+      <Titulo />    
+      <img src={jordan} className='tamanho' alt="imagem do michael jordan" />
+      <img src={michael} className='tamanho' alt="imagem michael jordan" />
+
+      <h2>Olá meu nome é: {nome}</h2>
+
+      <p>Exibição da soma de um numero 5 + 10 = {5+10}</p>
+
+      <p>soma usando função {somatoria(8,12)}</p>
     </>
   )
 }
---
+++ desafio_prof/src/assets/Componentes/Titulo.jsx
@@ -0,0 +1,13 @@
+import logo from '../imagens/logo.jpg'
+
+
+
+function titulo(){
+	return(
+		<header>
+		<img src={logo} alt="logo nike" />
+		<h1>Nike</h1>
+		</header>
+	)
+}
+export default titulo
