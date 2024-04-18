5048b1cc46cdd73890eca5e0bac6f68b70b83730
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-04-18 10:45:02 -0300

    .

--- desafio_prof/src/assets/Componentes/Titulo.jsx
+++ desafio_prof/src/assets/Componentes/Titulo.jsx
@@ -5,8 +5,16 @@ import './titulo.module.css'
 function titulo(){
 	return(
 		<header>
-		<img src={logo} alt="logo nike" />
-		<h1>Nike</h1>
+		<div>
+			<img src={logo} alt="logo nike" />
+			<h1>NIKE</h1>
+		</div>
+		<nav>
+			<a href="">Eportivos</a>
+			<a href="">Jordan</a>
+			<a href="">Strett</a>
+			<a href="">Contate nós</a>
+		</nav>
 		</header>
 	)
 }
